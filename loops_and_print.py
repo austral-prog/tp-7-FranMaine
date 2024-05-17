0fc9cfda3a9a5de796d57e1185d485d2ec14cc1f
FranMaine <162723272+FranMaine@users.noreply.github.com>
2024-05-17 12:32:21 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,21 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+    result = []
+    for index, value in enumerate(lista):
+        if value:
+            result.append(f"{len(result)}. {value}")
+    return result
+
+colors = ["Red", "Green", "", "White", "Black"]
+enumerated_colors = enumerate_list(colors)
+print(enumerated_colors)
 
 
-def enumerate_backwards(list):
-    return list
+
+def enumerate_backwards(lista):
+    result=[]
+    for index, color in enumerate(lista):
+        if color:
+            result.append(f'{len(result)}. {color[::-1]}')
+    return result
+colors = ["Red", "Green", "", "White", "Black"]
+print(enumerate_backwards(colors))
